327edca7a9cb6026c9710aa65dbed6e1df0cec0c
catalinflorea <floreacatalin94@gmail.com>
2019-11-06 18:34:21 +0200

    Add random numbers exercise

---
+++ Python-Basics/exercises/e3_random_numbers.py
@@ -0,0 +1,11 @@
+# import random
+from random import random, randrange
+
+#prob = random.random()
+prob = random()
+print(prob)
+
+# return an int, one of 1,2,3,4,5,6,7
+#diceThrow = random.randrange(1,8)
+diceThrow = randrange(1,8)
+print(diceThrow)
