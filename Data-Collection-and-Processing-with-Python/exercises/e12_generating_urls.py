98a5e1132ff0b700dc637295e8197f8c94a2d4a2
catalinflorea <floreacatalin94@gmail.com>
2020-01-17 17:25:45 +0200

    Generate urls using the requests.get function

---
+++ Data-Collection-and-Processing-with-Python/exercises/e12_generating_urls.py
@@ -0,0 +1,33 @@
+import requests
+
+# The HTTP protocol contains some restrictions regarding what characters can be used as part of a URL.
+# For example, a URL cannot contain any spaces, / or : characters. For this reason, there are special combinations of
+# characters designated as replacements for these forbidden characters.
+# More specifically:
+# a space is encoded as +
+# " is encoded as %22
+# : is encoded as %3A
+# ...and so on.
+
+# Fortunately, when we want to pass information as a URL parameter value, we don’t have to remember all the
+# substitutions that are required to encode special characters. Instead, that capability is built into the requests
+# module.
+
+# The get function in the requests module takes an optional parameter called params. If a value is specified for that
+# parameter, it should be a dictionary. The keys and values in that dictionary are used to append something to the URL
+# that is requested from the remote site.
+
+d = {'q': '"violins and guitars"', 'tbm': 'isch'}
+results = requests.get("https://google.com/search", params=d)
+print(results.url)  # https://www.google.com/search?q=%22violins+and+guitars%22&tbm=isch
+# Because dictionary keys are unordered in python, the final url might sometimes have the encoded key-value pairs in a
+# completely random order. Fortunately, most websites that accept URL parameters in this form will accept the key-value
+# pairs in any order.
+
+# Let's see another example:
+kval_pairs = {'rel_rhy': 'funny'}
+page = requests.get("https://api.datamuse.com/words", params=kval_pairs)
+# Let's print the first 50 characters:
+print(page.text[:50])  # [{"word":"money","score":4415,"numSyllables":2},{"
+# We print the url that was fetched:
+print(page.url)  # https://api.datamuse.com/words?rel_rhy=funny
